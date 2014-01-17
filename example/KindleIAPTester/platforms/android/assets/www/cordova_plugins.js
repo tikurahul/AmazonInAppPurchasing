3591dc94f7dbc6353f455adabbb3b54a997dea83
Yewon Lee <ywplee@gmail.com>
2014-01-16 17:48:16 -0800

    Initial commit, example not fully implemented

---
+++ example/KindleIAPTester/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.rahulrav.cordova.amazon.plugin.AmazonInAppPurchasing/www/amazonInApp.js",
+        "id": "com.rahulrav.cordova.amazon.plugin.AmazonInAppPurchasing.AmazonInAppPurchasing",
+        "clobbers": [
+            "AmazonInAppPurchasing"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.rahulrav.cordova.amazon.plugin.AmazonInAppPurchasing": "2.3.1"
+}
+// BOTTOM OF METADATA
+});
