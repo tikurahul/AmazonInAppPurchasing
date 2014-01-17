889ef0f6e68469061c88970a8a2f06525e0b2b3d
Yewon Lee <ywplee@gmail.com>
2014-01-16 20:42:25 -0800

    KINDLE IAP

--- example/KindleIAPTester/platforms/ios/.staging/www/cordova_plugins.js
+++
@@ -1,9 +0,0 @@
-cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
-module.exports.metadata = 
-// TOP OF METADATA
-{
-    "com.rahulrav.cordova.amazon.plugin.AmazonInAppPurchasing": "2.3.1"
-}
-// BOTTOM OF METADATA
-});
